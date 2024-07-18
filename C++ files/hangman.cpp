e4b94f82f418c2ef9deafc364b600a6b66c4b16a
Chi Ndeh-Zama <133240770+chizama20@users.noreply.github.com>
2024-07-18 12:54:38 -0400

    Update and rename hangman.cpp.cpp to hangman.cpp

--- C++ files/hangman.cpp
+++ C++ files/hangman.cpp
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -31,7 +24,7 @@ int main() {
     int n = rand() % 10;
     string word = words[n];
 
-    string hidden(word.length(), '*'); // Initialize hidden word with asterisks
+    string hidden(word.length(), '*'); 
     int strikes = 0;
     bool found;
     char guess;
